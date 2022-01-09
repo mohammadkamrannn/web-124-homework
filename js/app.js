1b7be952ce7fbfbb18cf017a923a2360f4fc55c9
Mohammad Kamran <mohammadkamrannn@gmail.com>
2022-01-09 15:50:17 +0330

    Edit innerText Code

--- js/app.js
+++ js/app.js
@@ -1,31 +1,37 @@
 document.getElementById("btn").onclick = function() {
+    
     var time = prompt("Please enter your time...");
+
     document.getElementById("btn").innerText = "ReEnter Time";
+
     var taskId = document.getElementById("task");
 
-    taskId.innerText = "";
+    task = "";
 
     if(time > 24 || time < 1 ){
-        taskId.innerText = "Please enter a time between 1 and 24";
+        task = "Please enter a time between 1 and 24";
     } else if(time < 8 || time == null || time > 22){
-        taskId.innerText = "You should be sleeping";
+        task = "You should be sleeping";
     } else if(time < 10){
-        taskId.innerText = "Wake up";
+        task = "Wake up";
     } else if(time < 12){
-        taskId.innerText = "Go to work";
+        task = "Go to work";
     } else if(time < 13){
-        taskId.innerText = "Lunch";
+        task = "Lunch";
     } else if(time < 17){
-        taskId.innerText = "Go to work";
+        task = "Go to work";
     } else if(time < 19){
-        taskId.innerText = "Gym";
+        task = "Gym";
     } else if(time < 20){
-        taskId.innerText = "Dinner Time";
+        task = "Dinner Time";
     } else if(time < 22){
-        taskId.innerText = "Watch TV";
+        task = "Watch TV";
     } else if(time = 22){
-        taskId.innerText = "Time to go to sleeeeep";
+        task = "Time to go to sleeeeep";
     }
+
+    taskId.innerText = task;
+
 }
 
 
