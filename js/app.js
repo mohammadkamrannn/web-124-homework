44043f6b657313238adc0e48e54aaa1e2a87c2ec
Mohammad Kamran <mohammadkamrannn@gmail.com>
2022-02-02 11:50:04 +0330

    add homework 06

--- js/app.js
+++ js/app.js
@@ -1,156 +1,54 @@
-var $taskCpu = document.getElementById("taskCpu");
-var $taskUser = document.getElementById("taskUser");
-var $countCpu = document.getElementById("countCpu");
-var $countUser = document.getElementById("countUser");
-var $winerLost = document.getElementById("winerLost");
+var numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+var suits = ['♠', '♥', '♦', '♣'];
+var cards = [];
 
-var taskCpu = "";
-var taskUser = "";
-var winerLost = "";
-var countCpu = 0;
-var countUser = 0;
+var $playerNum1 = document.getElementById('playerNum1');
+var $playerNum2 = document.getElementById('playerNum2');
+var $playerNum3 = document.getElementById('playerNum3');
+var $playerNum4 = document.getElementById('playerNum4');
 
-var $sang = 0;
-var $kaghaz = 1;
-var $gheychi = 2;
-
-document.getElementById("sang").onclick = function() {
-var rand = Math.floor(Math.random() * 3);    
-if(countCpu == 0 && countUser == 0){
-    $winerLost.innerHTML = "بازی کن";
-}
-switch(rand){
-    case $sang:
-        taskCpu = "کامپیوتر : " + "<i class='las la-fist-raised la-3x'></i>";
-        taskUser = "<i class='las la-fist-raised la-3x'></i>" + " : شما";
-        // winerLost = "مساوی";
-    break;
-    case $kaghaz:
-        taskCpu = "کامپیوتر : " + "<i class='las la-hand-paper la-3x'></i>";
-        taskUser = "<i class='las la-fist-raised la-3x'></i>" + " : شما";
-        countCpu += 1;
-        // winerLost = "باختی";
-    break;
-    case $gheychi:
-        taskCpu = "کامپیوتر : " + "<i class='las la-hand-peace la-3x'></i>";
-        taskUser = "<i class='las la-fist-raised la-3x'></i>" + " : شما";
-        countUser += 1;
-        // winerLost = "بردی";
-    break;
-}
-
-$taskCpu.innerHTML = taskCpu;
-$countCpu.innerHTML = countCpu;
-$taskUser.innerHTML = taskUser;
-$countUser.innerHTML = countUser;
-// $winerLost.innerHTML =winerLost;
-switch(5){
-    case countCpu:
-        $winerLost.innerHTML = "<div class='alert alert-danger' role='alert'>متاسفانه شما بازنده شدید!  <i class='las la-frown'></i></div>";
-        countCpu = 0;
-        countUser = 0;
-    break;
-    case countUser:
-        $winerLost.innerHTML = "<div class='alert alert-success' role='alert'>شما برنده شدید  <i class='las la-grin'></i></div>";
-        countCpu = 0;
-        countUser = 0;
-    break;
-}
-
+function makeCards() {
+    for(var i = 0; i < suits.length; i++) {
+        for(var j = 0; j < numbers.length; j++) {
+            cards.push(numbers[j] + suits[i]);
+        }
+    }
+    return cards;
 };
 
-document.getElementById("kaghaz").onclick = function() {
-var rand = Math.floor(Math.random() * 3);    
-if(countCpu == 0 && countUser == 0){
-    $winerLost.innerHTML = "بازی کن";
-}
-    
-switch(rand){
-    case $sang:
-        taskCpu = "کامپیوتر : " + "<i class='las la-fist-raised la-3x'></i>";
-        taskUser = "<i class='las la-hand-paper la-3x'></i>" + " : شما";
-        // winerLost = "بردی";
-        countUser += 1;
-    break;
-    case $kaghaz:
-        taskCpu = "کامپیوتر : " + "<i class='las la-hand-paper la-3x'></i>";
-        taskUser = "<i class='las la-hand-paper la-3x'></i>" + " : شما";
-        // winerLost = "مساوی";
-    break;
-    case $gheychi:
-        taskCpu = "کامپیوتر : " + "<i class='las la-hand-peace la-3x'></i>";
-        countCpu += 1;
-        taskUser = "<i class='las la-hand-paper la-3x'></i>" + " : شما";
-        // winerLost = "باختی";
-    break;
-}
-    
-$taskCpu.innerHTML = taskCpu;
-$countCpu.innerHTML = countCpu;
-$taskUser.innerHTML = taskUser;
-$countUser.innerHTML = countUser;
-// $winerLost.innerHTML =winerLost;
-switch(5){
-    case countCpu:
-        $winerLost.innerHTML = "<div class='alert alert-danger' role='alert'>متاسفانه شما بازنده شدید!  <i class='las la-frown'></i></div>";
-        countCpu = 0;
-        countUser = 0;
-    break;
-    case countUser:
-        $winerLost.innerHTML = "<div class='alert alert-success' role='alert'>شما برنده شدید  <i class='las la-grin'></i></div>";
-        countCpu = 0;
-        countUser = 0;
-    break;
-}
+function shuffle(arr) {
+    for (var i = arr.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
 };
 
-document.getElementById("gheychi").onclick = function() {
-var rand = Math.floor(Math.random() * 3);    
-if(countCpu == 0 && countUser == 0){
-    $winerLost.innerHTML = "بازی کن";
-}
-  
-switch(rand){
-        case $sang:
-        taskCpu = "کامپیوتر : " + "<i class='las la-fist-raised la-3x'></i>";
-        countCpu += 1;
-        taskUser = "<i class='las la-hand-peace la-3x'></i>" + " : شما";
-        // winerLost = "باختی";
-    break;
-    case $kaghaz:
-        taskCpu = "کامپیوتر : " + "<i class='las la-hand-paper la-3x'></i>";
-        taskUser = "<i class='las la-hand-peace la-3x'></i>" + " : شما";
-        // winerLost = "بردی";
-        countUser += 1;
-    break;
-    case $gheychi:
-        taskCpu = "کامپیوتر : " + "<i class='las la-hand-peace la-3x'></i>";
-        taskUser = "<i class='las la-hand-peace la-3x'></i>" + " : شما";
-        // winerLost = "مساوی";
-    break;
-}
-        
-$taskCpu.innerHTML = taskCpu;
-$countCpu.innerHTML = countCpu;
-$taskUser.innerHTML = taskUser;
-$countUser.innerHTML = countUser;
-// $winerLost.innerHTML =winerLost;
-switch(5){
-    case countCpu:
-        $winerLost.innerHTML = "<div class='alert alert-danger' role='alert'>متاسفانه شما بازنده شدید!  <i class='las la-frown'></i></div>";
-        countCpu = 0;
-        countUser = 0;
-    break;
-    case countUser:
-        $winerLost.innerHTML = "<div class='alert alert-success' role='alert'>شما برنده شدید  <i class='las la-grin'></i></div>";
-        countCpu = 0;
-        countUser = 0;
-    break;
-}
+document.getElementById('twoPlayer').onclick = function() {
+    cards = [];
+    cards = shuffle(makeCards());
+    $playerNum1.innerHTML = '<h3 class="text-center">بازیکن شماره 1</h3>' + '<div class="d-flex justify-content-center">' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[0] + '</p>' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[1] + '</p>' + '</div>';
+    $playerNum2.innerHTML = '<h3 class="text-center">بازیکن شماره 2</h3>' + '<div class="d-flex justify-content-center">' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[2] + '</p>' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[3] + '</p>' + '</div>';
+    $playerNum3.innerHTML = "";
+    $playerNum4.innerHTML = "";
 };
 
+document.getElementById('threePlayer').onclick = function() {
+    cards = [];
+    cards = shuffle(makeCards());
+    $playerNum1.innerHTML = '<h3 class="text-center">بازیکن شماره 1</h3>' + '<div class="d-flex justify-content-center">' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[0] + '</p>' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[1] + '</p>' + '</div>';
+    $playerNum2.innerHTML = '<h3 class="text-center">بازیکن شماره 2</h3>' + '<div class="d-flex justify-content-center">' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[2] + '</p>' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[3] + '</p>' + '</div>';
+    $playerNum3.innerHTML = '<h3 class="text-center">بازیکن شماره 3</h3>' + '<div class="d-flex justify-content-center">' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[4] + '</p>' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[5] + '</p>' + '</div>';
+    $playerNum4.innerHTML = "";
+};
 
-
-
-
-
+document.getElementById('fourPlayer').onclick = function() {
+    cards = [];
+    cards = shuffle(makeCards());
+    $playerNum1.innerHTML = '<h3 class="text-center">بازیکن شماره 1</h3>' + '<div class="d-flex justify-content-center">' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[0] + '</p>' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[1] + '</p>' + '</div>';
+    $playerNum2.innerHTML = '<h3 class="text-center">بازیکن شماره 2</h3>' + '<div class="d-flex justify-content-center">' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[2] + '</p>' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[3] + '</p>' + '</div>';
+    $playerNum3.innerHTML = '<h3 class="text-center">بازیکن شماره 3</h3>' + '<div class="d-flex justify-content-center">' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[4] + '</p>' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[5] + '</p>' + '</div>';
+    $playerNum4.innerHTML = '<h3 class="text-center">بازیکن شماره 4</h3>' + '<div class="d-flex justify-content-center">' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[6] + '</p>' + '<p class="display-4 bg-white rounded w-120-px py-5 m-1 text-center">' + cards[7] + '</p>' + '</div>';
+};
